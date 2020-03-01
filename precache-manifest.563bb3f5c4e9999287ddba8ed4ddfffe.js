be1bf04087267faf6b040b9690f94b94caa1a589
samsebamse <megasamsi@gmail.com>
2020-03-01 03:11:44 +0100

    Updates

---
+++ precache-manifest.563bb3f5c4e9999287ddba8ed4ddfffe.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e339d199c8c353a70ca73ad17f503d95",
+    "url": "/Samsebamse/samsebamse.github.io.git/index.html"
+  },
+  {
+    "revision": "a5f69a4fe7f3163c801c",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/css/main.f04c7dac.chunk.css"
+  },
+  {
+    "revision": "55b24cf30ac3ad4f9fd5",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/js/2.20a191cd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/js/2.20a191cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f69a4fe7f3163c801c",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/js/main.d752342b.chunk.js"
+  },
+  {
+    "revision": "60b9bd8be193b4c4c5b3",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/js/runtime-main.80afd84d.js"
+  },
+  {
+    "revision": "17bd466a557c2e9f8e1655f1257b70a4",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/blackwood2.17bd466a.jpg"
+  },
+  {
+    "revision": "365e60886739c55d7919ce8cff37b9d3",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconangular.365e6088.png"
+  },
+  {
+    "revision": "270a15916cae070a6bba9a31e8747568",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconangular_bw.270a1591.png"
+  },
+  {
+    "revision": "4d61f7b5405e64186e82b3a93c95e309",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconcsharp.4d61f7b5.png"
+  },
+  {
+    "revision": "c6e845aeabfd38da5618e282b7bdc945",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconcsharp_bw.c6e845ae.png"
+  },
+  {
+    "revision": "3c3ad0184d79da4f7b4f4e90466c4bc5",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconjava.3c3ad018.png"
+  },
+  {
+    "revision": "018b5e4efe0f9aaa19888ed1090a478e",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconjava_bw.018b5e4e.png"
+  },
+  {
+    "revision": "c2f77108916f406af6c4165dda9f5210",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconreact.c2f77108.png"
+  },
+  {
+    "revision": "82ad9fc81b47013dd8cdad2413056ae3",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/iconreact_bw.82ad9fc8.png"
+  },
+  {
+    "revision": "7f033a2f20fe8149752a317157908d96",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/portrait.7f033a2f.png"
+  },
+  {
+    "revision": "9445940d8d1980e7cff3098a2c9a791d",
+    "url": "/Samsebamse/samsebamse.github.io.git/static/media/project.9445940d.png"
+  }
+]);
